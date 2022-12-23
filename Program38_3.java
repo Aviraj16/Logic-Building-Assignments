4c174f300b8a14647ee8d6a8afa909a1e96b88e8
Aviraj Latpate <92982701+Aviraj16@users.noreply.github.com>
2022-12-23 23:02:02 +0530

    Add files via upload

---
+++ Program38_3.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Pattern
+{
+    public void DisplayPattern(String str)
+    {     
+        for(int i =0;i<=str.length()-1;i++)
+        { 
+            for(int j = 0;j<=str.length()-1;j++)
+            {
+                if(i>=j)
+                {
+                    System.out.print(str.charAt(j)+"\t");
+                }
+                
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+class Program38_3
+
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter String");
+        String str  = sobj.nextLine();
+
+        Pattern pobj = new Pattern();
+
+        pobj.DisplayPattern(str);
+
+    }
+}
---
+++ Program38_5.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Pattern
+{
+    public void DisplayPattern(String str)
+    {
+        for(int i =0;i<=str.length()-1;i++)
+        { 
+            for(int j = 0;j<=str.length()-1;j++)
+            {
+                if(i>=j)
+                {
+                    System.out.print(str.charAt(j)+"\t");
+                }
+                
+            } 
+            System.out.println();
+        }
+
+        for(int i =str.length()-2;i>=0;i--)
+        {
+            
+            for(int j = 0;j<=str.length()-1;j++)
+            {
+                if(i>=j)
+                {
+                    System.out.print(str.charAt(j)+"\t");
+                }
+                
+            }
+
+            System.out.println();
+        }    
+        
+
+    }
+
+}
+
+class Program38_5
+
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter String");
+        String str  = sobj.nextLine();
+
+        Pattern pobj = new Pattern();
+
+        pobj.DisplayPattern(str);
+
+    }
+}
+
+
